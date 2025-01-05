0d4d1295ca98f27b44c0788b63c314bb36cb2f3c
kwxsuper <60839509+kwxsuper@users.noreply.github.com>
2025-01-05 23:02:46 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["42.194.239.122"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["desk.kwx.me"];
 pub const PUBLIC_RS_PUB_KEY: &str = "nT2BR6bYASw91gLKooLPk1XImvNTa5JCXsyXvKjeqVc=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
